f782021f9c327af38e8aadaf91f6a125f9ee1b8b
zangchao <zangc@koal.com>
2023-03-27 15:01:57 +0800

    update

--- smf-sys/build/main.rs
+++ smf-sys/build/main.rs
@@ -4,14 +4,26 @@ use std::env;
 fn main(){
     // 参考 https://doc.rust-lang.org/cargo/reference/build-scripts.html
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let target_abi = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    
+    let mut platform = target_os;
+    
+    if target_arch == "wasm32" {
+        platform = "wasm".to_string();
+    }
 
-    let lib_dir = format!("smf-sdk/lib/{}/{}", target_os, target_abi);
+    let lib_dir = format!("smf-sdk/lib/{}/{}", platform, target_arch);
 
     println!("cargo:rustc-link-search=native={}", lib_dir);
-    println!("cargo:rustc-link-lib={}={}", "dylib", "smf_api");
+    
+    if target_arch == "wasm32" {
+        println!("cargo:rustc-link-lib={}", "smf_api");
+    }
+    else {
+        println!("cargo:rustc-link-lib={}={}", "dylib", "smf_api");
+    }
 
-    if target_abi == "x86" {
+    if platform == "x86" {
         // 条件编译
         println!("cargo:rustc-cfg=cfg_test");
     }
